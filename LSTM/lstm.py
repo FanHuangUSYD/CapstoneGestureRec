e65c1efa53b804f8f4a283615458aa0fac38b48b
Linun-OL <linunvan@outlook.com>
2024-04-30 23:02:15 +1000

    get some metric plots

--- LSTM/lstm.py
+++ LSTM/lstm.py
@@ -1,15 +1,16 @@
 import os
 
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 
 from LSTM.DataGenerator.get_ecg_data import get_data_loader
-from LSTM.util import get_directory, device
+from LSTM.util import get_directory, device, plot_accuracy, plot_confusion_matrix
 
 
 class LSTMModel(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size: int, hidden_size: int, output_size: int):
         super(LSTMModel, self).__init__()
         self.hidden_size = hidden_size
         self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True)
@@ -27,8 +28,8 @@ class LSTMModel(nn.Module):
         return out
 
 
-def train_model(model, train_loader, val_loader, num_epochs: int = 100, learning_rate: float = 0.001,
-                early_stop_epochs: int = 20, train_name: str = "test"):
+def train_model(model, train_loader, val_loader, test_loader, name_list: list[str], num_epochs: int = 200, learning_rate: float = 0.001,
+                early_stop_epochs: int = 30, train_name: str = "test"):
     model.to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
@@ -36,7 +37,13 @@ def train_model(model, train_loader, val_loader, num_epochs: int = 100, learning
     best_accuracy = 0.0
     no_improvement_count = 0
 
+    train_accuracies = []  # List to store training accuracies
+    val_accuracies = []  # List to store validation accuracies
+
     save_path = get_directory()
+
+    best_model = os.path.join(save_path, train_name + '_best.pt')
+    last_model = os.path.join(save_path, train_name + '_last.pt')
 
     for epoch in range(num_epochs):
         model.train()
@@ -59,11 +66,13 @@ def train_model(model, train_loader, val_loader, num_epochs: int = 100, learning
 
         val_accuracy = evaluate_model(model, val_loader)
         print(f'Epoch [{epoch + 1}/{num_epochs}], Train Accuracy: {accuracy:.4f} | Validation Accuracy: {val_accuracy:.4f}')
+        train_accuracies.append(accuracy)
+        val_accuracies.append(val_accuracy)
 
         if val_accuracy > best_accuracy:
             best_accuracy = val_accuracy
             no_improvement_count = 0
-            torch.save(model, os.path.join(save_path, train_name + '_best.pt'))  # Save the best model
+            torch.save(model, best_model)  # Save the best model
         else:
             no_improvement_count += 1
 
@@ -75,7 +84,9 @@ def train_model(model, train_loader, val_loader, num_epochs: int = 100, learning
                   f'Best Accuracy: {best_accuracy:.4f}')
             break
 
-    torch.save(model, os.path.join(save_path, train_name + '_last.pt'))  # Save the final model
+    torch.save(model, last_model)  # Save the final model
+    plot_accuracy({"Training Dataset": train_accuracies, "Validation Dataset": val_accuracies})
+    test_model(best_model, output_size, test_loader, name_list)
     return
 
 
@@ -93,6 +104,32 @@ def evaluate_model(model, dataloader) -> float:
     return accuracy
 
 
+def test_model(model_path: str, output_size: int, dataloader, name_list: list[str]) -> None:
+
+    target_model = torch.load(model_path)
+    target_model.eval()
+
+    confusion_matrix = np.zeros((output_size, output_size), dtype=float)
+
+    with torch.no_grad():
+        for inputs, targets in dataloader:
+            outputs = target_model(inputs)
+            _, predicted = torch.max(outputs, 1)
+
+            for i in range(len(targets)):
+                confusion_matrix[predicted[i], targets[i]] += 1
+
+    # column sum
+    col_sum = confusion_matrix.sum(axis=0)
+    # Iterate through each element and divide by the sum of its column
+    for i in range(output_size):
+        confusion_matrix[:, i] /= col_sum[i]
+
+    # Plot the confusion matrix
+    plot_confusion_matrix(confusion_matrix, name_list)
+    return
+
+
 if __name__ == '__main__':
     # initialize model
     input_size = 2
@@ -103,19 +140,23 @@ if __name__ == '__main__':
     seq_length = 4000
     batch_size = 10
 
+    # labels
+    name_list = ["ECG Data"] + ["Mock Data " + str(i) for i in range(1, output_size)]
+
     # train model
     # num_epochs = 100
     # learning_rate = 0.001
 
     model = LSTMModel(input_size, hidden_size, output_size)
 
-    train_loader = get_data_loader(num_samples=6000, feature_num=input_size, seq_length=seq_length, batch_size=batch_size, data_class=output_size)
-    val_loader = get_data_loader(num_samples=1000, feature_num=input_size, seq_length=seq_length, batch_size=batch_size, data_class=output_size)
+    train_loader = get_data_loader(name_list, num_samples=6000, feature_num=input_size, seq_length=seq_length,
+                                   batch_size=batch_size, data_class=output_size)
 
-    train_model(model, train_loader, val_loader)
+    val_loader = get_data_loader(name_list, num_samples=2000, feature_num=input_size, seq_length=seq_length,
+                                 batch_size=batch_size, data_class=output_size, tag="Validation Dataset")
 
-# 实验；
-# train dta 周期  normal distribu(bias)
-# 一段一段测，（长的截取）设定长度的大小（希望小的也性能好）段的大小，
+    test_loader = get_data_loader(name_list, num_samples=1000, feature_num=input_size, seq_length=seq_length,
+                                  batch_size=batch_size, data_class=output_size, tag="Test Dataset")
 
-# 训练数据用不同长度 （dimension 一样 ）
+    train_model(model, train_loader, val_loader, test_loader, name_list, train_name="LSTM_Validation")
+
--- LSTM/util.py
+++ LSTM/util.py
@@ -1,12 +1,16 @@
 import os
 from collections import Counter
 from datetime import datetime
+from typing import Dict, List
 
+import numpy as np
+import seaborn as sns
 import torch
 from matplotlib import pyplot as plt
 from torch import device
 
 ROOT = os.path.dirname(__file__)
+
 
 def get_directory() -> str:
     now = datetime.now()
@@ -20,6 +24,9 @@ def get_directory() -> str:
     return directory_path
 
 
+output_dir = get_directory()
+
+
 def get_device() -> device:
     if torch.cuda.is_available():
         device = torch.device('cuda')
@@ -28,6 +35,9 @@ def get_device() -> device:
         device = torch.device('cpu')
         print('CUDA is not available. Using CPU.')
     return device
+
+
+device = get_device()
 
 
 def plot_data_frequency(label_list: list[int], name_list: list[str] = None, tag: str = None) -> None:
@@ -42,14 +52,58 @@ def plot_data_frequency(label_list: list[int], name_list: list[str] = None, tag:
     plt.bar(labels, counts)
     plt.xlabel(None)
     plt.ylabel('counts')
-    plt.title(f'{tag if tag else "Dataset"}.png')
-    plt.ylim(int(min(counts)*0.85), int(max(counts)*1.05))
+    plt.title(f'{tag if tag else "Dataset"}')
+    plt.ylim(int(min(counts) * 0.85), int(max(counts) * 1.05))
     for index, count in enumerate(counts):
         plt.text(index, count + 0.1, str(count), ha='center', va='bottom')
 
     plt.tight_layout()
-    plt.savefig(os.path.join(get_directory(), f'class_frequency{"_in_" + tag if tag else ""}.png'))
+    plt.savefig(os.path.join(output_dir, f'class_frequency{" in " + tag if tag else ""}.png'))
     # plt.show()
 
 
-device = get_device()
+def plot_accuracy(accuracies: Dict[str, List[float]]) -> None:
+    np.savez(os.path.join(output_dir, "accuracy.npz"), **accuracies)
+    plt.figure(figsize=(8, 5))
+
+    epochs = range(1, len(accuracies[list(accuracies.keys())[0]]) + 1)
+    for key, values in accuracies.items():
+        plt.plot(epochs, values, marker='o', label=f'{key}')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.title(f'Accuracy on training and validation Dataset')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(os.path.join(output_dir, f'accuracy_of_lstm.png'))
+    # plt.show()
+    return
+
+
+def plot_confusion_matrix(confusion_matrix: np.ndarray, name_list: list[str]) -> None:
+    """
+    Plot the confusion matrix as a heatmap.
+
+    Parameters:
+    - confusion_matrix (np.ndarray): The confusion matrix.
+    - output_size (int): Size of the output labels.
+
+    Returns:
+    - None
+    """
+    np.savez(os.path.join(output_dir, "confusion_matrix.npz"), confusion_matrix)
+    plt.figure(figsize=(12, 9))
+    sns.heatmap(confusion_matrix, annot=True, fmt='.2f', cmap='Blues')
+    plt.xlabel('True Labels')
+    plt.ylabel('Predicted Labels')
+    plt.title('Confusion Matrix')
+    if len(name_list) == len(confusion_matrix):
+        plt.xticks(ticks=[index + 0.5 for index in range(len(name_list))], labels=name_list)
+        plt.yticks(ticks=[index + 0.5 for index in range(len(name_list))], labels=name_list)
+    plt.savefig(os.path.join(output_dir, f'confusion_matrix_of_lstm.png'))
+    # plt.show()
+    return
+
+
+if __name__ == '__main__':
+    plot_accuracy({"Training Dataset": [0.5, 0.4, 0.7, 0.3, 0.6, 0.3, 0.4, 0.8],
+                   "Validation Dataset": [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.3]})
